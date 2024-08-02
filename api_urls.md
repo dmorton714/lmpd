88abfdec11f7c83fa173a571936c4d3b332045bd
dmorton714 <danny.morton714@gmail.com>
2024-08-02 10:57:52 -0400

    working on new API cant test due to slow internet

---
+++ api_urls.md
@@ -0,0 +1,33 @@
+2015 - 2017 
+Url - https://data.louisvilleky.gov/datasets/LOJIC::louisville-metro-ky-lmpd-stops-data-2015-2017/explore 
+API URL - https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_LMPD_Stops_Data_20152017/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
+
+2021 
+url - https://data.louisvilleky.gov/datasets/55503adae05c413896e490154173d4db_0/explore
+API URL - https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_2021/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
+
+2009 - 2021 
+url - https://data.louisvilleky.gov/datasets/LOJIC::louisville-metro-ky-lmpd-stops-data-from-2009-12-12-2021/explore
+api url - https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOPS_DATA_(2)/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
+
+2020 
+url - https://data.louisvilleky.gov/datasets/LOJIC::louisville-metro-ky-lmpd-stops-data-2020/explore
+api url - https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_2020_(2)/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
+
+2023 
+url - https://data.louisvilleky.gov/datasets/LOJIC::louisville-metro-ky-lmpd-stops-data-2023/explore
+api url - https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_2023/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
+
+2022
+url - https://data.louisvilleky.gov/datasets/b0c3b853420a4a00b974027b6197cb80_0/explore
+api url - https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_202/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
+
+
+urls = [
+  # 2009 - 2021
+  "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOPS_DATA_(2)/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+  # 2022
+  "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_202/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+  # 2023
+  "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_2023/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+]
--- data_builder.py
+++ data_builder.py
@@ -14,147 +14,108 @@ from api_key import api_key
 # - Takes 16 mins to run returns 1.5 million rows          #
 # -------------------------------------------------------- #
 
-# urls = [
-#     'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2023/FeatureServer/0', # noqa
-#     'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2022/FeatureServer/0', # noqa
-#     'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2021/FeatureServer/0', # noqa
-#     'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2020/FeatureServer/0', # noqa
-#     'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/UniformCitationData_2016_2019/FeatureServer/0', # noqa
-#     'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/UniformCitationData_2012_2015/FeatureServer/0' # noqa
-# ]
+urls = [
+    'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2023/FeatureServer/0', # noqa
+    'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2022/FeatureServer/0', # noqa
+    'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2021/FeatureServer/0', # noqa
+    'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Louisville_Metro_KY_Uniform_Citation_Data_2020/FeatureServer/0', # noqa
+    'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/UniformCitationData_2016_2019/FeatureServer/0', # noqa
+    'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/UniformCitationData_2012_2015/FeatureServer/0' # noqa
+]
 
-# batch_size = 1000
-# data_list = []
+batch_size = 1000
+data_list = []
 
-# for url in urls:
-#     offset = 0
+for url in urls:
+    offset = 0
 
-#     while True:
-#         params = {
-#             'where': '1=1',
-#             'outFields': '*',
-#             'returnGeometry': 'false',
-#             'resultOffset': offset,
-#             'resultRecordCount': batch_size,
-#             'f': 'json'
-#         }
+    while True:
+        params = {
+            'where': '1=1',
+            'outFields': '*',
+            'returnGeometry': 'false',
+            'resultOffset': offset,
+            'resultRecordCount': batch_size,
+            'f': 'json'
+        }
 
-#         response = requests.get(f"{url}/query", params=params)
-#         response.raise_for_status()
+        response = requests.get(f"{url}/query", params=params)
+        response.raise_for_status()
 
-#         query_result = response.json()
-#         features = query_result.get('features', [])
+        query_result = response.json()
+        features = query_result.get('features', [])
 
-#         for feature in features:
-#             data_list.append(feature['attributes'])
+        for feature in features:
+            data_list.append(feature['attributes'])
 
-#         if len(features) == 0:
-#             break
+        if len(features) == 0:
+            break
 
-#         offset += batch_size
+        offset += batch_size
 
-# df = pd.DataFrame(data_list)
+df = pd.DataFrame(data_list)
 
-# output_directory = 'data'
+output_directory = 'data'
 
-# os.makedirs(output_directory, exist_ok=True)
+os.makedirs(output_directory, exist_ok=True)
 
-# df.to_csv(os.path.join(output_directory, 'citation.csv'), index=False)
+df.to_csv(os.path.join(output_directory, 'citation.csv'), index=False)
 
-# print(f"Data saved to {os.path.join(output_directory, 'citation.csv')}") # noqa
+print(f"Data saved to {os.path.join(output_directory, 'citation.csv')}") # noqa
 
 
 # -------------------------------------------------------- #
 # LMPD Employee data                                       #
 # -------------------------------------------------------- #
 
-# urls = ['https://opendata.arcgis.com/datasets/beecd7d525ed44b6af7f75b114e35d53_0/FeatureServer/0']  # noqa
-
-# batch_size = 1000
-# data_list = []
-
-# for url in urls:
-#     offset = 0
-
-#     while True:
-#         params = {
-#             'where': '1=1',
-#             'outFields': '*',
-#             'returnGeometry': 'false',
-#             'resultOffset': offset,
-#             'resultRecordCount': batch_size,
-#             'f': 'json'
-#         }
-
-#         response = requests.get(f"{url}/query", params=params)
-#         response.raise_for_status()
-
-#         query_result = response.json()
-#         features = query_result.get('features', [])
-
-#         for feature in features:
-#             data_list.append(feature['attributes'])
-
-#         if len(features) == 0:
-#             break
-
-#         offset += batch_size
-
-# df = pd.DataFrame(data_list)
-
-# output_directory = 'data'
-# os.makedirs(output_directory, exist_ok=True)
-
-# df.to_csv(os.path.join(output_directory, 'employee.csv'), index=False)
-
-# print(f"Data saved to {os.path.join(output_directory, 'employee.csv')}")
+https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/Gun_Violence_Data/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
 
 # -------------------------------------------------------- #
 # citation 2019 in old project scope                       #
 # -------------------------------------------------------- #
 
-# urls = [
-#     'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_2019_(2)/FeatureServer/0'] 
+urls = [
+    'https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/LMPD_STOP_DATA_2019_(2)/FeatureServer/0'] 
 
-# batch_size = 1000
-# data_list = []
+batch_size = 1000
+data_list = []
 
-# for url in urls:
-#     offset = 0
+for url in urls:
+    offset = 0
 
-#     while True:
-#         params = {
-#             'where': '1=1',
-#             'outFields': '*',
-#             'returnGeometry': 'false',
-#             'resultOffset': offset,
-#             'resultRecordCount': batch_size,
-#             'f': 'json'
-#         }
+    while True:
+        params = {
+            'where': '1=1',
+            'outFields': '*',
+            'returnGeometry': 'false',
+            'resultOffset': offset,
+            'resultRecordCount': batch_size,
+            'f': 'json'
+        }
 
-#         response = requests.get(f"{url}/query", params=params)
-#         response.raise_for_status()
+        response = requests.get(f"{url}/query", params=params)
+        response.raise_for_status()
 
-#         query_result = response.json()
-#         features = query_result.get('features', [])
+        query_result = response.json()
+        features = query_result.get('features', [])
 
-#         for feature in features:
-#             data_list.append(feature['attributes'])
+        for feature in features:
+            data_list.append(feature['attributes'])
 
-#         if len(features) < batch_size:
-#             break
+        if len(features) < batch_size:
+            break
 
-#         offset += batch_size
+        offset += batch_size
 
-# df = pd.DataFrame(data_list)
+df = pd.DataFrame(data_list)
 
-# output_directory = 'data'
-# os.makedirs(output_directory, exist_ok=True)
+output_directory = 'data'
+os.makedirs(output_directory, exist_ok=True)
 
-# output_file = os.path.join(output_directory, 'citation_2019.csv')
-# df.to_csv(output_file, index=False)
+output_file = os.path.join(output_directory, 'citation_2019.csv')
+df.to_csv(output_file, index=False)
 
-# print(f"Data saved to {output_file}")
+print(f"Data saved to {output_file}")
 
 
 # -------------------------------------------------------- #
